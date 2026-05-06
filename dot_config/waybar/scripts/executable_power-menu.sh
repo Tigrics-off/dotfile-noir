7186e7fc7df88fcc792b4ee21935afb15d44450a
Tigrics <tarasdarovtigran@gmail.com>
2026-05-06 21:13:02 +0300

    Fix pow menu (again)

--- dot_config/waybar/scripts/executable_power-menu.sh
+++ dot_config/waybar/scripts/executable_power-menu.sh
@@ -2,7 +2,7 @@
 
 entries="󰐥 Shutdown\n󰜉 Reboot\n󰤄 Suspend\n󰈆 Logout"
 
-selected=$(echo -e $entries | wofi --dmenu --conf /dev/null --style /dev/null --width 200 --height 250 --cache-file /dev/null | awk '{print tolower($2)}')
+selected=$(echo -e $entries | wofi --dmenu | awk '{print tolower($2)}')
 
 case $selected in
   shutdown)
